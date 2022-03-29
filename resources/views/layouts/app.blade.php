30300db0b3c3984e8d497f9ce3322ba6c8d9d6ed
luthfihrdy <72455807+zhenaz@users.noreply.github.com>
2022-03-29 11:29:34 +0700

    add preloader

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,9 +35,9 @@
 <div class="wrapper">
 
   <!-- Preloader -->
-  <!-- <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-  </div> -->
+ <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__wobble" src="{{asset('img/logo.png')}}" alt="PKCMampangPrapatanLogo" height="60" width="60">
+  </div>
 
   <!-- Navbar -->
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -93,8 +93,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="{{asset('img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">SPA</span>
+      <img src="{{asset('img/logo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Sigara PKC Mampang</span>
     </a>
 
     <!-- Sidebar -->
