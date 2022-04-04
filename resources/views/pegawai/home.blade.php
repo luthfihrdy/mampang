57d515e68ee3fcf803494de17ba78dcc33f4636f
luthfihrdy <72455807+zhenaz@users.noreply.github.com>
2022-04-04 14:44:04 +0700

    lottie

--- resources/views/pegawai/home.blade.php
+++ resources/views/pegawai/home.blade.php
@@ -18,7 +18,7 @@
         <div class="col-sm-12">
           <div class="card bg-info rounded">
               <div class="row card-body ">
-                 <div class="col-sm-8">
+                 <div class="col-sm-6">
                     <h5 class="card-title"><input type="date" class="form-control bg-white" style="opacity: 0.5"></h5>
                     <br/><br/><br/><br/><br/><br/>
                     <p class="card-text"><h2>Good Day,   
@@ -29,9 +29,9 @@
                     @endphp {{ $name[0] }} </h2></p>
                     <p class="card-text">Have a nice day!</p>
                   </div>
-                  <div class="col-sm-4">
+                  <div class="col-sm-6">
                     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-                    <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_pzprncar.json"  background="transparent"  speed="1" loop  autoplay style="height:350px;"></lottie-player>
+                    <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_pzprncar.json"  background="transparent"  speed="1" loop  autoplay style="height:400px;"></lottie-player>
                     {{-- <img src="{{asset('img/hello.png')}}" alt="doctor" height="250"/> --}}
                   </div>
               </div>
@@ -43,16 +43,33 @@
         <div class="col-md-4">
           <div class="card bg-info rounded">
               <div class="row card-body">
-                 <div class="col-sm-8">
+                 <div class="col-sm-12">
                     <h5 class="card-title">
                     {{-- <input type="date" class="form-control bg-white" style="opacity: 0.5"></h5> --}}
-                    <br/><br/><br/><br/><br/><br/>
-                    <p class="card-text"><h2>Birthday Today! </h2></p>
-                    <p class="card-text">Have a nice day!</p>
-                  </div>
                   <div class="col-sm-4">
-                    <img class="img-fluid" src="{{asset('img/hello.png')}}" alt="doctor" height="50"/>
+                  <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                  <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_IVkOMK.json"  background="transparent"  speed="1"  style="width: 150px; height: 150px;"  loop  autoplay></lottie-player>
                   </div>
+                  <br/><br/><br/>
+                    <p class="card-text"><h2>Birthday Today!</h2></p><br/>
+                    <p class="card-text">None.</p>
+                  </div>
+              </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card bg-info rounded">
+              <div class="row card-body">
+                 <div class="col-sm-12">
+                    <h5 class="card-title">
+                    {{-- <input type="date" class="form-control bg-white" style="opacity: 0.5"></h5> --}}
+                    <br/>
+                    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                    <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_x9h8ar8l.json"  background="transparent"  speed="1"  style="width: 120px; height: 120px;"  loop autoplay></lottie-player>
+                    <br/><br/><br/>
+                    <p class="card-text"><h2>Cuti </h2></p><br/>
+                    <p class="card-text">None.</p>
+                    </div>
               </div>
           </div>
         </div>
@@ -65,25 +82,8 @@
                     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
                     <lottie-player src="https://assets8.lottiefiles.com/private_files/lf30_zzItmF.json"  background="transparent"  speed="1"  style="width: 180px; height: 180px;"  loop  autoplay></lottie-player>
                     <br/><br/><br/>
-                    <p class="card-text"><h2>Cuti </h2></p><br/>
+                    <p class="card-text"><h2>Sakit <br/> Ijin </h2></p><br/>
                     <p class="card-text">None.</p>
-                    </div>
-              </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="card bg-info rounded">
-              <div class="row card-body">
-                 <div class="col-sm-8">
-                    <h5 class="card-title">
-                    {{-- <input type="date" class="form-control bg-white" style="opacity: 0.5"></h5> --}}
-                    <br/><br/><br/><br/><br/><br/>
-                    <p class="card-text"><h2>Sakit / Izin</h2></p>
-                    <p class="card-text">Have a nice day!</p>
-                  </div>
-                  <div class="col-sm-4">
-                    <img class="img-fluid" src="{{asset('img/hello.png')}}" alt="doctor" height="50"/>
-                  </div>
               </div>
           </div>
         </div>
