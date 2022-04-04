fb9959d8cdf6bfdb3ec57842d218f2bec67a1ba4
M Luthfi Hardiansyah <66915287+luthfihrdy@users.noreply.github.com>
2022-04-04 16:01:27 +0700

    Fix height card

--- resources/views/pegawai/home.blade.php
+++ resources/views/pegawai/home.blade.php
@@ -30,7 +30,6 @@
                     <p class="card-text">Have a nice day!</p>
                   </div>
                   <div class="col-sm-6">
-                    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
                     <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_pzprncar.json"  background="transparent"  speed="1" loop  autoplay style="height:400px;"></lottie-player>
                     {{-- <img src="{{asset('img/hello.png')}}" alt="doctor" height="250"/> --}}
                   </div>
@@ -38,59 +37,69 @@
           </div>
         </div>
       </div><!-- end row -->
-      <!-- row -->
+
+      <!--row -->
       <div class="row">
-        <div class="col-md-4">
-          <div class="card bg-info rounded">
-              <div class="row card-body">
-                 <div class="col-sm-12">
-                    <h5 class="card-title">
-                    {{-- <input type="date" class="form-control bg-white" style="opacity: 0.5"></h5> --}}
-                  <div class="col-sm-4">
-                  <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        <div class="col-md-4 mb-3">
+          <div class="card bg-info rounded h-100">
+            <div class="row card-body">
+              <div class="col-6 col-sm-6">
+                <h5 class="card-title">
                   <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_IVkOMK.json"  background="transparent"  speed="1"  style="width: 150px; height: 150px;"  loop  autoplay></lottie-player>
-                  </div>
-                  <br/>
-                    <p class="card-text"><h2>Birthday Today!</h2></p><br/>
-                    <p class="card-text">None.</p><br/>
-                    <button type="button" class="btn btn-light" data-dismiss="modal" id="close-modal">More</button>
-                  </div>
+                </h5>
               </div>
+              <div class="col-6 col-sm-6">
+                <h2 class="py-3">Birthday Today!</h2>
+              </div>
+              
+            </div>
+            
+            <div class="card-body">
+              None.
+            </div>
           </div>
         </div>
-        <div class="col-md-4">
-          <div class="card bg-info rounded">
-              <div class="row card-body">
-                 <div class="col-sm-12">
-                    <h5 class="card-title">
-                    {{-- <input type="date" class="form-control bg-white" style="opacity: 0.5"></h5> --}}
-                    <br/>
-                    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-                    <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_x9h8ar8l.json"  background="transparent"  speed="1"  style="width: 120px; height: 120px;"  loop autoplay></lottie-player>
-                    <br/><br/>
-                    <p class="card-text"><h2>Cuti <br/> </h2></p><br/>
-                    <p class="card-text">None.</p><br/>
-                    <button type="button" class="btn btn-light" data-dismiss="modal" id="close-modal">More</button>
-                    </div>
+
+        <div class="col-md-4 mb-3">
+          <div class="card bg-info rounded h-100">
+            <div class="row card-body">
+              <div class="col-6 col-sm-6">
+                <h5 class="card-title">
+                  <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_x9h8ar8l.json"  background="transparent"  speed="1"  style="width: 130px; height: 130px;"  loop autoplay></lottie-player>
+                </h5>
               </div>
+              <div class="col-6 col-sm-6">
+                <h2 class="py-3">Cuti</h2>
+              </div>
+              
+            </div>
+            
+            <div class="card-body">
+              None.
+            </div>
           </div>
         </div>
-        <div class="col-md-4">
-          <div class="card bg-info rounded">
-              <div class="row card-body">
-                 <div class="col-sm-12">
-                    <h5 class="card-title">
-                    {{-- <input type="date" class="form-control bg-white" style="opacity: 0.5"></h5> --}}
-                    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-                    <lottie-player src="https://assets8.lottiefiles.com/private_files/lf30_zzItmF.json"  background="transparent"  speed="1"  style="width: 180px; height: 180px;"  loop  autoplay></lottie-player>
-                    <p class="card-text"><h2>Sakit <br/> Ijin </h2></p><br/>
-                    <p class="card-text">None.</p><br/>
-                    <button type="button" class="btn btn-light" data-dismiss="modal" id="close-modal">More</button>
+
+        <div class="col-md-4 mb-3">
+          <div class="card bg-info rounded h-100">
+            <div class="row card-body">
+              <div class="col-6 col-sm-6">
+                <h5 class="card-title">
+                  <lottie-player src="https://assets8.lottiefiles.com/private_files/lf30_zzItmF.json"  background="transparent"  speed="1"  style="width: 150px; height: 150px;"  loop  autoplay></lottie-player>
+                </h5>
               </div>
+              <div class="col-6 col-sm-6">
+                <h2 class="py-3">Sakit / Ijin</h2>
+              </div>
+              
+            </div>
+            
+            <div class="card-body">
+              None.
+            </div>
           </div>
         </div>
-        
-      </div><!-- end row -->
+      </div><!--end row-->
       
     </div><!-- /.container-fluid -->
 </div>
@@ -125,4 +134,7 @@
 </div><!-- /.container-fluid -->
 </section>
 <!-- /.content -->
+@endsection
+@section('script')
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
 @endsection
