9ee85d69ecafe99b0163992d156e11810ebdd5d0
luthfihrdy <72455807+zhenaz@users.noreply.github.com>
2022-04-08 13:13:45 +0700

    view skp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Dashboard</title>
+  <title>PKC Mampang Prapatan</title>
   <!-- icon -->
   <link rel="icon" href="{{asset('img/logo.png')}}">
 
