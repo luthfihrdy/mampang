32e1d548a8c95c2db29d6b7800760ce086cbbef8
luthfihrdy <72455807+zhenaz@users.noreply.github.com>
2022-04-08 14:19:16 +0700

    skp validator pdf print
    pluginnya banyak banget:'v

--- resources/views/pegawai/skp.blade.php
+++ resources/views/pegawai/skp.blade.php
@@ -212,6 +212,19 @@
 @section('script')
 
 <script>
+let dateDropdown = document.getElementById('date-dropdown');
+
+    let currentYear = new Date().getFullYear();
+    let earliestYear = 2020;
+
+    while (currentYear >= earliestYear) {
+      let dateOption = document.createElement('option');
+      dateOption.text = currentYear;
+      dateOption.value = currentYear;
+      dateDropdown.add(dateOption);
+      currentYear -= 1;
+    }
+
 </script>
 
 @endsection
--- resources/views/validator/skpvalidasi.blade.php
+++ resources/views/validator/skpvalidasi.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
+<head>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{asset('css/all.min.css')}}">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{asset('css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/responsive.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/buttons.bootstrap4.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}">
+</head>
 
 @section('content')
 <!-- Content Header (Page header) -->
@@ -116,29 +128,28 @@
 
 @section('script')
 <!-- jQuery -->
-<script src="../../plugins/jquery/jquery.min.js"></script>
+<script src="{{asset('jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 <!-- DataTables  & Plugins -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="../../plugins/jszip/jszip.min.js"></script>
-<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
-<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<script src="{{asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('plugins/djs/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/datatabljs/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('plugins/datatabljs/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/datatabljs/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('plugins/datatabljs/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('plugins/jszip/jszip.min.js')}}"></script>
+<script src="{{asset('plugins/pdfmake/pdfmake.min.js')}}"></script>
+<script src="{{asset('plugins/pdfmake/vfs_fonts.js')}}"></script>
+<script src="{{asset('plugins/datatabljs/buttons.html5.min.js')}}"></script>
+<script src="{{asset('plugins/datatabljs/buttons.print.min.js')}}"></script>
+<script src="{{asset('plugins/datatabljs/buttons.colVis.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="../../dist/js/adminlte.min.js"></script>
+<script src="{{asset('js/adminlte.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../../dist/js/demo.js"></script>
+<script src="{{asset('js/demo.js')}}"></script>
 <!-- Page specific script -->
 <script>
-
   $(function () {
     $("#example1").DataTable({
       "responsive": true, "lengthChange": false, "autoWidth": false,
@@ -154,201 +165,6 @@
       "responsive": true,
     });
   });
-
-$(document).ready(function(){
-
-    dTable = $('#myTable').DataTable({
-        columnDefs: [
-            { width: '12%', targets:1},
-            { width: '5%', targets:2},
-            { width: '5%', targets:3},
-            { width: '40%', targets:4}
-        ],
-        // order: [[2,'asc']],
-        responsive: true,
-        processing: true,
-        "language": {
-            "processing": "<img style='width:150px;' src='{{asset('img/loader-transparent.gif')}}' />" //add a loading image,simply putting <img src="loader.gif" /> tag.
-        },
-        serverSide: true,
-        ajax: "{{ route('validator.aktivitas_get')}}",
-        columns: [
-            // { data: 'IdType', name: 'IdType' },
-            {
-                data: 'get_user',
-                render: function(data, type, row, meta){
-                    if(data == null){
-                            return null;
-                        }else{
-                            return data.name;
-                    }
-                }
-            },
-            {
-                data: 'tanggal_kegiatan',
-                name: 'tanggal_kegiatan'
-            },
-            {
-                data: 'jam_awal',
-                name: 'jam_awal'
-            },
-            {
-                data: 'jam_akhir',
-                name: 'jam_akhir'
-            },
-            {
-                data: 'kegiatan',
-                name: 'kegiatan'
-            },
-            // {
-            //     data: 'point_menit',
-            //     name: 'point_menit'
-            // },
-            {
-                data: 'status',
-                render: function(data, type, row) {
-                    if(row.status == 'Waiting') {
-                        return '<span class="badge badge-warning">' + data + '</span>'
-                    }else if(row.status == 'Approved') {
-                        return '<span class="badge badge-success">' + data + '</span>'
-                    }else if(row.status == 'Rejected') {
-                        return '<span class="badge badge-danger">' + data + '</span>'
-                    }
-                }
-            },
-            // {
-            //     data: 'role_id',
-            //     render: function(data, type, row){
-            //         if(row.role_id == 1) {
-            //             return 'Admin';
-            //         }else if(row.role_id == 2){
-            //             return 'Validator';
-            //         }else if(row.role_id == 3){
-            //             return 'Pegawai';
-            //         }
-            //     }
-            // },
-            // {
-            //     data: 'role_id',
-            //     name: 'role_id'
-            // },
-            {
-                data: 'id',
-                //name: 'id'
-                render: function (data, type, row) {
-                    if(row.status == 'Approved' || row.status == 'Rejected') {
-                        let buttonApprove = '<button type="button" class="ml-2 btn btn-success btn-sm disabled" onclick="buttonApprove(\'' + data + '\')"><i class="fas fa-check"></i></button>'
-                        let buttonReject = '<button type="button" class="ml-2 btn btn-danger btn-sm disabled" onclick="buttonReject(\'' + data + '\')"><i class="fa fa-times"></i></button>'
-                        return buttonApprove + buttonReject;
-                    }
-                    if(row.status == 'Waiting') {
-                        let buttonApprove = '<button type="button" class="ml-2 btn btn-success btn-sm" onclick="buttonApprove(\'' + data + '\')"><i class="fas fa-check"></i></button>'
-                        let buttonReject = '<button type="button" class="ml-2 btn btn-danger btn-sm" onclick="buttonReject(\'' + data + '\')"><i class="fa fa-times"></i></button>'
-                        return buttonApprove + buttonReject;
-                    }//let buttonApprove = '<button href="#" class="text-success ml-2 disabled" onclick="buttonApprove(\'' + data + '\')"><i class="fas fa-check"></i></button>';
-                    //return buttonApprove + buttonReject;
-                }
-            }
-            ]
-    });
-
-});
-
-function buttonApprove(ids) {
-        swal.fire({
-            title: "Approve?",
-            icon: 'question',
-            text: "Apakah anda yakin ingin mengkonfirmasi data?",
-            type: "warning",
-            showCancelButton: !0,
-            confirmButtonText: "Ya Konfirmasi!",
-            cancelButtonText: "Batal",
-            reverseButtons: !0,
-        }).then(function (e) {
-
-            if (e.value === true) {
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $("input[name='_token']").val()
-                    }
-                });
-                // let token = $('meta[name="csrf-token"]').attr('content');
-                
-                let _url = `/validator/aktivitas/approve/${ids}`;
-
-                $.ajax({
-                    type: 'POST',
-                    url: _url,
-                    data: {ids: ids},
-                    success: function (resp) {
-                        if (resp.success) {
-                            swal.fire("Done!", resp.message, "success");
-                            dTable.ajax.reload();
-                        } else {
-                            swal.fire("Error!", 'Something went wrong.', "error");
-                        }
-                    },
-                    error: function (resp) {
-                        swal.fire("Error!", 'Something went wrong.', "error");
-                    }
-                });
-
-            } else {
-                e.dismiss;
-            }
-
-        }, function (dismiss) {
-            return false;
-        })
-    }
-
-    function buttonReject(ids) {
-        swal.fire({
-            title: "Tolak?",
-            icon: 'question',
-            text: "Apakah anda yakin ingin Menolak data?",
-            type: "warning",
-            showCancelButton: !0,
-            confirmButtonText: "Ya Tolak!",
-            cancelButtonText: "Batal",
-            reverseButtons: !0,
-        }).then(function (e) {
-
-            if (e.value === true) {
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $("input[name='_token']").val()
-                    }
-                });
-                // let token = $('meta[name="csrf-token"]').attr('content');
-                let _url = `/validator/aktivitas/reject/${ids}`;
-
-                $.ajax({
-                    type: 'POST',
-                    url: _url,
-                    data: {ids: ids},
-                    success: function (resp) {
-                        if (resp.success) {
-                            swal.fire("Done!", resp.message, "success");
-                            dTable.ajax.reload();
-                        } else {
-                            swal.fire("Error!", 'Something went wrong.', "error");
-                        }
-                    },
-                    error: function (resp) {
-                        swal.fire("Error!", 'Something went wrong.', "error");
-                    }
-                });
-
-            } else {
-                e.dismiss;
-            }
-
-        }, function (dismiss) {
-            return false;
-        })
-    }
-
 </script>
 
 @endsection
