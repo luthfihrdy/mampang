20e5c6d48672ab1f03a1910a7179c1af5c636848
M Luthfi Hardiansyah <66915287+luthfihrdy@users.noreply.github.com>
2022-04-27 09:23:53 +0700

    fix user admin

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -1139,11 +1139,25 @@
                 },
                 {
                     data: 'fasyankes',
-                    name: 'fasyankes'
+                    render: function(data, type, row, meta) {
+                        if(data == null){
+                            return '-';
+                        }else {
+                            return data;
+                        }
+                        
+                    }
                 },
                 {
                     data: 'unit_kerja',
-                    name: 'unit_kerja',
+                    render: function(data, type, row, meta) {
+                        if(data == null){
+                            return '-';
+                        }else {
+                            return data;
+                        }
+                        
+                    }
                 },
                 // {
                 //     data: 'role_id',
